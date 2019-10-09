e1bdfd3db40b6e79c340f0f152041ff464feb3e2
github username <github email address>
2019-10-09 11:55:23 -0700

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
 # code here!
+require "pry"
+class School
+
+    attr_reader :roster, :school_name
+
+    def initialize(school_name)
+        @school_name = school_name
+        @roster = {}
+    end
+
+    def add_student(name, grade)
+        @roster[grade] ? @roster[grade] << name : @roster[grade] = [name]
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        @roster.keys.each do |grade|
+            @roster[grade].sort!
+        end
+        @roster.sort.to_h
+    end
+end
+
+hth = School.new("High Tech High")
+#binding.pry
